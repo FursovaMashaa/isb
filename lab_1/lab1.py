256a007ab1a93ce1a55479ae6974586802528f9f
FursovaMashaa <fursovamaria1@gmail.com>
2024-05-20 21:35:03 +0400

    task1 seems to be ready

--- lab_1/lab1.py
+++ lab_1/lab1.py
@@ -1,5 +1,7 @@
 import json
 
+
+
 def read_txt(file_name: str) -> str:
     try:
         with open(file_name, 'r', encoding='utf-8') as file:
@@ -10,9 +12,11 @@ def read_txt(file_name: str) -> str:
         print ("the file was not found")
     except Exception as e:
         print(f"error: {e}")
+    print("Успешно")
+    
     
 
-def read_json(file_name: str) -> 'dict[str, str]':
+def read_json(file_name: str) -> dict[str, str]:
     try:
         with open(file_name, 'r', encoding='utf-8') as file:
             text = json.load(file)
@@ -21,6 +25,7 @@ def read_json(file_name: str) -> 'dict[str, str]':
         print ("the file was not found")
     except Exception as e:
         print(f"error: {e}")
+    print("Успешно")
     
 
 def write_txt(file_name: str, text: str) -> None:
@@ -31,12 +36,22 @@ def write_txt(file_name: str, text: str) -> None:
         print(f"An error occurred while writing to the file: {e}")
 
 
-def main() -> None:
-    file_name_txt = 'lab_1/task1/original_text.txt'
-    read_txt(file_name_txt)
+def caesar_cipher(text: str, key: int) -> str:
+    alphabet = "АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ "
+    shifted_alphabet = alphabet[key:] + alphabet[:key]
+    table = str.maketrans(alphabet, shifted_alphabet)
+    return text.translate(table)
 
-    file_name_json = 'lab_1/task1/key.json'
-    read_txt(file_name_json)
+
+def main():
+    input_text = read_txt('C:\\Users\\furso\\Desktop\\isb\\lab_1\\task1\\original_text.txt')
+    json_data = read_json('C:\\Users\\furso\\Desktop\\isb\\lab_1\\task1\\key.json')
+    key = int(json_data["caesar_cipher_key"])
+    encrypted_text = caesar_cipher(input_text, key)
+    write_txt('C:\\Users\\furso\\Desktop\\isb\\lab_1\\task1\\encrypted.txt', encrypted_text)
+    print("Успешно")
+  
+
 
 
 
