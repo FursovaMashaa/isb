ddca61220e3ba9f18a2f6cff842b038a731f5555
FursovaMashaa <fursovamaria1@gmail.com>
2024-09-10 18:56:14 +0400

    the last test

--- lab_2/NIST tests.py
+++ lab_2/NIST tests.py
@@ -1,9 +1,9 @@
 import json
 import math
+import mpmath
 
-from const import PATH
+from const import PATH, PI
 
-from mpmath import erfc
 
 
 def read_json(file_name: str) -> dict[str, str] | None:
@@ -55,6 +55,41 @@ def the_same_consecutive_bits(bits):
 
 
 
+def max_consecutive_ones(block: str) -> int:
+    max_count = 0
+    current_count = 0
+    for bit in block:
+        if bit == '1':
+            current_count += 1
+            max_count = max(max_count, current_count)
+        else:
+            current_count = 0
+    return max_count
+
+def analyze_sequence(sequence: str) -> float:
+    block_size = 8
+    blocks = [sequence[i:i + block_size] for i in range(0, len(sequence), block_size)]
+    V = [0] * 4  
+    for block in blocks:
+            max_length = max_consecutive_ones(block)
+            
+            if max_length == 0 or max_length == 1:
+                V[0] += 1
+            elif max_length == 2:
+                V[1] += 1
+            elif max_length == 3:
+                V[2] += 1
+            elif max_length > 3:
+                V[3] += 1
+    
+
+    x_squared = sum((V[i] - 16 * PI[i]) ** 2 / (16 * PI[i]) for i in range(4))
+
+    p_value = mpmath.gammainc(1.5, x_squared / 2)
+
+    return p_value
+
+
 def main() -> None:
     java_data = read_json(PATH)
 
@@ -72,5 +107,8 @@ def main() -> None:
     else:
         print(f"P-значение: {p_value}")   
 
+    input_sequence = "11010110"
+    result = analyze_sequence(input_sequence)
+    print("p-значение:", result)
 if __name__ == "__main__":
     main()
